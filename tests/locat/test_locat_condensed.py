9a0dd83e79894b9c072ddaa1d85d387f6d084c02
Francesco Strino <francesco.strino@pcmgf.com>
2026-02-10 14:20:27 +0200

    added tests

---
+++ tests/locat/test_locat_condensed.py
@@ -0,0 +1,12 @@
+import unittest
+import jax
+
+
+class AlgorithmTestCase(unittest.TestCase):
+    def test_jax_devices(self):
+        jax_devices = jax.devices()
+        self.assertIsNotNone(jax_devices)
+
+
+if __name__ == '__main__':
+    unittest.main()
