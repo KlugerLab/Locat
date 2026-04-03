e9c81331722b025cc9e10382721b9dbad0c75aa0
Francesco Strino <francesco.strino@pcmgf.com>
2026-04-03 18:31:58 +0200

    changed sidak implementation to remove warnings

--- tests/test_locat.py
+++ tests/test_locat.py
@@ -19,7 +19,9 @@ class LocatTestCase(unittest.TestCase):
 
         self.assertAlmostEqual(np.exp(logsidak_from_logp(np.log(0.01), 8)), 0.077255305572 , places=8)
 
-        self.assertAlmostEqual(_safe_p(logsidak_from_logp(-56.24362650, 8)), 1-1e-15 ,
+        self.assertAlmostEqual(_safe_p(logsidak_from_logp(56.24362650, 8)), 1-1e-15 ,
+                               places=8, msg="very large p-values are clipped in the pipeline" )
+        self.assertAlmostEqual(_safe_p(logsidak_from_logp(-56.24362650, 8)), 1e-15 ,
                                places=8, msg="very small p-values are clipped in the pipeline" )
 
         self.assertAlmostEqual(logsidak_from_logp(-56.24362650, 8), -54.164184960 ,
